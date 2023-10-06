a174e24d3ace5d40b847db0d1b9d51ef6be6a248
marshall <marshallxie16@gmail.com>
2023-10-05 21:47:47 -0700

    Heroku final final final

--- emailmodels/settings.py
+++ emailmodels/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['emailmodels.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['emailmodels-aaae8958e05a.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
